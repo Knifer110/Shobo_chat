1f1216ed2a08dc1ca60dbba5fdd34a6bc0c194ba
K <ee177093@meiji.ac.jp>
2019-09-17 17:42:37 +0900

    バグの修正

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const moment = require('moment');
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('example.db');
 
-app.use(express.static('html'));
+app.use(express.static(__dirname));
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.sendFile('index.html');
+    res.sendFile(__dirname + '/html/index.html');
 });
 
 app.post('/', (req, res) => {
@@ -20,39 +20,52 @@ app.post('/', (req, res) => {
     const user = req.body.name;
     const text = req.body.text;
     function chatSave(){
-        db.serialize(() => {
-            db.run('CREATE TABLE IF NOT EXISTS messages (date TEXT, user TEXT, content TEXT)');
-       	const add = db.prepare('INSERT INTO messages (date, user, content) VALUES (?, ?, ?)');
-            add.run([`${date}`, `${user}`, `${text}`]);
-            add.finalize();
-        });
-    }    
-        chatSave();
-        res.status(200);
-        res.sendFile('index.html');
-});
-
-app.get('/database', (req, res) => {
-    function chatLoad{
-         db.serialize(() => {
-            db.all('SELECT * FROM messages ORDER BY date DESC', (error, row) => {
-                if(error){
-                    console.log('ERROR!', error);
-                    return;
-                }
-            return row;
+        return new Promise(resolve => {
+            db.serialize(() => {
+                db.run('CREATE TABLE IF NOT EXISTS messages (date TEXT, user TEXT, content TEXT)');
+                const add = db.prepare('INSERT INTO messages (date, user, content) VALUES (?, ?, ?)');
+                add.run([`${date}`, `${user}`, `${text}`]);
+                add.finalize( () => resolve());
             });
         });
     }
+
+    async function chatPost(){
+        await chatSave();
+        res.status(200);
+        res.sendFile(__dirname + '/html/index.html');
+    }
+
+    chatPost();
+});
+
+
+app.get('/database', (req, res) => {
+
+    function chatLoad(){
+        return new Promise(resolve => {
+            db.serialize(() => {
+                db.all('SELECT * FROM messages ORDER BY date DESC', (error, row) => {
+                    if(error){
+                        console.log('ERROR!', error);
+                        return;
+                    }
+                    resolve(row);
+                });
+            });
+        });
+    }
+
     async function chatSend(){
         const text = await chatLoad();
-        await res.send(text);
+        res.send(text);
     }
+
     chatSend();
 });
 
 app.listen(3000, () => console.log('Express app listening on port 3000!'));
 //終了時の処理
 process.on('beforeExit', () =>{
-	db.close();
+    db.close();
 });
