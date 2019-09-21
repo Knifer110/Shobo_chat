d7da485144ae6ef93d9032e5e3e4e5a5438c4608
K <ee177093@meiji.ac.jp>
2019-09-21 11:11:08 +0900

    即時実行関数に変更

--- app.js
+++ app.js
@@ -1,71 +1,69 @@
-'use strict'
+"use strict";
 
-const express=require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 //データベース用
-const moment = require('moment');
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('example.db');
+const moment = require("moment");
+const sqlite3 = require("sqlite3").verbose();
+const db = new sqlite3.Database("example.db");
 
 app.use(express.static(__dirname));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/html/index.html');
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/html/index.html");
 });
 
-app.post('/', (req, res) => {
-    const date = moment().format('YYYY-MM-DD HH:mm:s');
-    const user = req.body.name;
-    const text = req.body.text;
-    function chatSave(){
-        return new Promise(resolve => {
-            db.serialize(() => {
-                db.run('CREATE TABLE IF NOT EXISTS messages (date TEXT, user TEXT, content TEXT)');
-                const add = db.prepare('INSERT INTO messages (date, user, content) VALUES (?, ?, ?)');
-                add.run([`${date}`, `${user}`, `${text}`]);
-                add.finalize( () => resolve());
-            });
+app.post("/", (req, res) => {
+  const date = moment().format("YYYY-MM-DD HH:mm:s");
+  const user = req.body.name;
+  const text = req.body.text;
+  function chatSave() {
+    return new Promise(resolve => {
+      db.serialize(() => {
+        db.run(
+          "CREATE TABLE IF NOT EXISTS messages (date TEXT, user TEXT, content TEXT)"
+        );
+        const add = db.prepare(
+          "INSERT INTO messages (date, user, content) VALUES (?, ?, ?)"
+        );
+        add.run([`${date}`, `${user}`, `${text}`]);
+        add.finalize(() => resolve());
+      });
+    });
+  }
+
+  (async function chatPost() {
+    await chatSave();
+    res.status(200);
+    res.sendFile(__dirname + "/html/index.html");
+  })();
+});
+
+app.get("/database", (req, res) => {
+  function chatLoad() {
+    return new Promise(resolve => {
+      db.serialize(() => {
+        db.all("SELECT * FROM messages ORDER BY date DESC", (error, row) => {
+          if (error) {
+            console.log("ERROR!", error);
+            return;
+          }
+          resolve(row);
         });
-    }
+      });
+    });
+  }
 
-    async function chatPost(){
-        await chatSave();
-        res.status(200);
-        res.sendFile(__dirname + '/html/index.html');
-    }
-
-    chatPost();
+  (async function chatSend() {
+    const text = await chatLoad();
+    res.send(text);
+  })();
 });
 
-
-app.get('/database', (req, res) => {
-
-    function chatLoad(){
-        return new Promise(resolve => {
-            db.serialize(() => {
-                db.all('SELECT * FROM messages ORDER BY date DESC', (error, row) => {
-                    if(error){
-                        console.log('ERROR!', error);
-                        return;
-                    }
-                    resolve(row);
-                });
-            });
-        });
-    }
-
-    async function chatSend(){
-        const text = await chatLoad();
-        res.send(text);
-    }
-
-    chatSend();
-});
-
-app.listen(3000, () => console.log('Express app listening on port 3000!'));
+app.listen(3000, () => console.log("Express app listening on port 3000!"));
 //終了時の処理
-process.on('beforeExit', () =>{
-    db.close();
+process.on("beforeExit", () => {
+  db.close();
 });
